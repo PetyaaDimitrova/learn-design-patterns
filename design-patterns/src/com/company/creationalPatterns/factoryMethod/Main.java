f68086e374b8147ad937281f74dc0c59d871a0df
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-10-31 13:02:20 +0200

    Add files via upload

---
+++ design-patterns/src/com/company/creationalPatterns/factoryMethod/Main.java
@@ -0,0 +1,11 @@
+package com.company.creationalPatterns.factoryMethod;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NotificationFactory notificationFactory = new NotificationFactory();
+        Notification notification = notificationFactory.createNotification("SMS");
+        notification.notifyUser();
+
+    }
+}
