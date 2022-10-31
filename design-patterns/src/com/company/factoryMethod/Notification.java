be1c2eaf08efddf299c5becc01f8efb3ce46d24d
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-10-31 13:22:38 +0200

    Delete design-patterns/src/com/company/factoryMethod directory

--- design-patterns/src/com/company/factoryMethod/Notification.java
+++
@@ -1,6 +0,0 @@
-package com.company.creationalPatterns.factoryMethod;
-
-public interface Notification {
-
-     void notifyUser();
-}
--- design-patterns/src/com/company/factoryMethod/SMSNotification.java
+++
@@ -1,8 +0,0 @@
-package com.company.creationalPatterns.factoryMethod;
-
-public class SMSNotification implements Notification{
-    @Override
-    public void notifyUser() {
-        System.out.println("Sending an SMS notification");
-    }
-}
